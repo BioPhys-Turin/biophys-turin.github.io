88ddf0859bef055968ab379d7aacbc14bf77a43e
Filippo Valle <filippo.valle@unito.it>
2025-06-05 16:08:01 +0200

    imporve coords

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([45.05, 7.68], 10);
+var map = L.map('map').setView([45.05, 7.68], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
@@ -12,7 +12,7 @@ var redMarker = L.AwesomeMarkers.icon({
     markerColor: 'darkpurple'
 });
 
-var marker = L.marker([45.05232742060916, 7.681427509114751], { icon: redMarker }).addTo(map);
+var marker = L.marker([45.0521, 7.6818], { icon: redMarker }).addTo(map);
 
 var popup = L.popup()
     .setLatLng([45.0524072060916, 7.681427509114751])
